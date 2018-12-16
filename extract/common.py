46c521386ef2852ffdfa4bf2331623c81577d012
Oswaldo Cruz <oswaldo.cruz.simon@bbva.com>
2018-12-16 14:05:25 -0600

    Added login page class

--- extract/common.py
+++ extract/common.py
@@ -1,5 +1,9 @@
 import yaml
+from selenium import webdriver
+
 __config = None
+__credentials = None
+__browser = None
 
 def config():
     global __config
@@ -7,3 +11,16 @@ def config():
         with open('config.yaml', mode='r') as f:
             __config = yaml.load(f)
     return __config
+
+def credentials():
+    global __credentials
+    if not __credentials:
+        with open('.credentials.yaml', mode='r') as f:
+            __credentials = yaml.load(f)
+    return __credentials
+
+def browser():
+    global __browser
+    if not __browser:
+        __browser = webdriver.Chrome(config()['driver']['path'])
+    return __browser
